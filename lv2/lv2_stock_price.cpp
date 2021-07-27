ada115068bb0a3f2168467e843901bfc0519f6ff
hshs1103 <hshs1103@naver.com>
2021-07-27 20:59:12 +0900

    level 2 stock price

---
+++ lv2/lv2_stock_price.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size(), 0);
+    
+    for(int i =0; i< prices.size(); i++){
+        for(int j=i+1; j < prices.size(); j++){
+            answer[i] = j - i;
+            if(prices[i] > prices[j]) break;
+        }
+    }
+    return answer;
+}
